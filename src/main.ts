9b11696675cd2ff1df9d7dda33db8d1cb1effafc
Akop Kesheshyan <hello@akop.dev>
2024-02-13 16:56:08 +0100

    Add support for citation and bibliography templates

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
-import { FileSuggest } from './suggest/FolderSuggester';
+import { FileSuggest, CSLSuggest } from './suggest';
 import { parse } from "@retorquere/bibtex-parser";
 import { Entry } from "@retorquere/bibtex-parser/grammar";
+import { CitationGenerator } from "./citation-generator"
 
-interface BibTeXManagerSettings {
-	mySetting: string;
+interface BibTexManagerSettings {
+	cslStyle: string,
+	templateArticle: string,
+	templateBook: string,
+	templateBooklet: string,
+	templateConference: string,
+	templateInbook: string,
+	templateIncollection: string,
+	templateInproceedings: string,
+	templateManual: string,
+	templateMastersthesis: string,
+	templateMisc: string,
+	templatePhdthesis: string,
+	templateProceedings: string,
+	templateTechreport: string,
+	templateUnpublished: string
+
 }
 
-const DEFAULT_SETTINGS: BibTeXManagerSettings = {
+const DEFAULT_SETTINGS: BibTexManagerSettings = {
+	cslStyle: "apa-6th-edition",
 	templateArticle: "",
 	templateBook: "",
 	templateBooklet: "",
@@ -24,7 +41,7 @@ const DEFAULT_SETTINGS: BibTeXManagerSettings = {
 	templateUnpublished: ""
 }
 
-const BIBTEX_TYPES = [
+const BIBTEX_TYPES: Array<string> = [
 	"Article",
 	"Book",
 	"Booklet",
@@ -41,21 +58,63 @@ const BIBTEX_TYPES = [
 	"Unpublished"
 ]
 
+export abstract class StringHelper {
+	public static sanitizeKeyString(key: string): string {
+		const allLowerCase = key.toLowerCase();
+		return allLowerCase.charAt(0).toUpperCase() + allLowerCase.slice(1);
+	}
+
+	public static trim(str: string, trim: string): string {
+		let start = 0;
+		let end = str.length;
+
+		while (start < end && str[start] === trim)
+			++start;
+
+		while (end > start && str[end - 1] === trim)
+			--end;
+
+		return (start > 0 || end < str.length) ? str.substring(start, end) : str;
+	}
+}
 export default class BibTeXManager extends Plugin {
-	settings: BibTeXManagerSettings;
+	settings: BibTexManagerSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		this.addCommand({
 			id: 'insert-bibtex',
-			name: 'Insert bibliography',
+			name: 'Insert',
 			callback: () => {
 				new InsertBibTexModal(this.app, this.settings).open();
 			}
 		});
 
 		this.addSettingTab(new BibTeXManagerSettingTab(this.app, this));
+
+		this.registerMarkdownCodeBlockProcessor("bibtex", async (source: string, el, ctx) => {
+			const codeBlock = el.createEl("div").createEl("pre").createEl("code");
+
+			try {
+
+				console.log(this.settings.cslStyle)
+				const generator = new CitationGenerator(this.settings.cslStyle, false);
+				await generator.createEngine();
+
+				const text = parse(source)
+				// @ts-ignore
+				text.entries.forEach(async (entry: Entry) => {
+					generator.addCitation(entry);
+				})
+				const bib = generator.getBibliography()
+				codeBlock.createEl("span", { text: bib, cls: "bibtex key" });
+
+			} catch (exception) {
+				codeBlock.createEl("span", { text: "Invalid BibTeX format!", cls: "bibtex key" });
+			}
+		});
+
 	}
 
 	async loadSettings() {
@@ -68,18 +127,18 @@ export default class BibTeXManager extends Plugin {
 }
 
 class InsertBibTexModal extends Modal {
-	bibliography: string;
+	bibtex: string;
 	template: string;
-	settings: BibTeXManagerSettings;
+	settings: BibTexManagerSettings;
 
-	constructor(app: App, settings: BibTeXManagerSettings) {
+	constructor(app: App, settings: BibTexManagerSettings) {
 		super(app);
 		this.settings = settings;
 	}
 
 	async getTemplate(entry: Entry): Promise<string> {
-		const key = "template" + entry.type.charAt(0).toUpperCase() + entry.type.slice(1);
-		let template = this.template ? this.settings["template"+this.template] : this.settings[key];
+		const templateName = entry.type.charAt(0).toUpperCase() + entry.type.slice(1);
+		let template = this.template ? this.settings["template" + this.template] : this.settings["template" + templateName];
 		if (!template) {
 			return "";
 		}
@@ -91,15 +150,35 @@ class InsertBibTexModal extends Modal {
 	}
 
 	async onSubmit(text: string) {
-		const editor = this.app.workspace.getActiveViewOfType(MarkdownView).editor;
+		const viewer = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (!viewer) {
+			new Notice("No active editor");
+			return;
+		}
 		try {
+			const generator = new CitationGenerator(this.settings.cslStyle, false);
+
+			// @ts-ignore
 			parse(text).entries.forEach(async (entry: Entry) => {
 				let file = await this.getTemplate(entry);
-				console.log(file)
-				Object.keys(entry.fields).forEach(key => {
-					file = file.replace("{{" + key + "}}", entry.fields[key]);
-				})
-				editor.replaceSelection(file+"\n");
+				if (file) {
+					await generator.createEngine();
+					// generate citation
+					generator.addCitation(entry);
+
+					const mapping = {
+						"type": entry.type,
+						"citekey": (<any>entry).key,
+						"bibliography": generator.getBibliography(),
+						"citation": generator.getCitation((<any>entry).key)
+					}
+
+					const fields = Object.assign(mapping, entry.fields);
+					Object.keys(fields).forEach(key => {
+						file = file.replace("{{" + key + "}}", fields[key]);
+					})
+					viewer.editor.replaceSelection(file + "\n");
+				}
 			})
 		} catch (e) {
 			console.error(e);
@@ -109,34 +188,33 @@ class InsertBibTexModal extends Modal {
 
 	}
 
-	onOpen() {
+	async onOpen() {
 		const { contentEl } = this;
 
+		contentEl.createEl("h2", { text: "Insert BibTeX" });
 
-		contentEl.createEl("h1", { text: "Bibliography" });
 
 		new Setting(contentEl)
-			.setName('Template')
+			.addTextArea(text => {
+				text.setPlaceholder('Paste the content of your BibTeX file')
+					.onChange((value) => {
+						this.bibtex = value
+					});
+				text.inputEl.addClass("bibtex-manager-template");
+			}).setClass("bibtex-manager-template-container");
+
+		new Setting(contentEl)
+			.setName('Apply template')
 			.addDropdown((cb) => {
-				cb.addOption("", "Auto");
-				cb.addOptions(BIBTEX_TYPES, BIBTEX_TYPES)
+				cb.addOption("", "Auto detect");
+				BIBTEX_TYPES.forEach((type) =>
+					cb.addOption(type, type));
+
 				cb.onChange((value) => {
-					if (value != "") {
-						value = BIBTEX_TYPES[value]
-					}
-					this.template = value;
+					this.template = value ? BIBTEX_TYPES[value] : "";
 				});
 			});
 
-		new Setting(contentEl)
-			.setName('BibTex')
-			.addTextArea(text => {
-				text.setPlaceholder('Enter your bibliography here')
-					.onChange((value) => {
-						this.bibliography = value
-					});
-				text.inputEl.addClass("bibtex_manager_bibliography");
-			})
 
 		new Setting(contentEl)
 			.addButton((btn) => {
@@ -152,7 +230,7 @@ class InsertBibTexModal extends Modal {
 					.setCta()
 					.onClick(() => {
 						this.close();
-						this.onSubmit(this.bibliography);
+						this.onSubmit(this.bibtex);
 					}));
 
 	}
@@ -169,6 +247,7 @@ class BibTeXManagerSettingTab extends PluginSettingTab {
 	constructor(app: App, plugin: BibTeXManager) {
 		super(app, plugin);
 		this.plugin = plugin;
+		this.app = app;
 	}
 
 	display(): void {
@@ -176,21 +255,36 @@ class BibTeXManagerSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl("h1", { text: "Templates" });
 
+		new Setting(this.containerEl)
+			.setName("Citation style ID")
+			.addSearch((cb) => {
+				new CSLSuggest(this.app, cb.inputEl);
+				cb.setPlaceholder("CSL Style: style-name")
+					.setValue(this.plugin.settings.cslStyle)
+					.onChange((style) => {
+						this.plugin.settings.cslStyle = style
+						this.plugin.saveSettings();
+
+					});
+				// @ts-ignore
+				cb.containerEl.addClass("bibtex-manager-search");
+			});
+
+		containerEl.createEl('h2', { text: "Templates" });
 
 		BIBTEX_TYPES.forEach((type) => {
 			new Setting(containerEl)
 				.setName(type)
 				.addSearch((cb) => {
-					new FileSuggest(cb.inputEl);
+					new FileSuggest(this.app, cb.inputEl);
 					cb.setValue(this.plugin.settings[`template${type}`])
 						.onChange((folder) => {
 							this.plugin.settings[`template${type}`] = folder;
 							this.plugin.saveSettings();
 						});
 					// @ts-ignore
-					cb.containerEl.addClass("bibtex_manager_search");
+					cb.containerEl.addClass("bibtex-manager-search");
 				});
 		});
 	}
---
+++ src/suggest/csl.ts
@@ -0,0 +1,26 @@
+import { TextInputSuggest } from "./suggest";
+import { cslList, CSLItem } from "../utils";
+
+export class CSLSuggest extends TextInputSuggest<CSLItem> {
+	getSuggestions(inputStr: string): CSLItem[] {
+		const lowerCaseInputStr = inputStr.toLowerCase();
+		const items: Array<CSLItem> = [];
+		const listItem = cslList.forEach((item: CSLItem) => {
+			if (item.label.toLowerCase().contains(lowerCaseInputStr)) {
+				items.push(item);
+			}
+		});
+
+		return items
+	}
+
+	renderSuggestion(item: CSLItem, el: HTMLElement): void {
+		el.setText(item.label);
+	}
+
+	selectSuggestion(item: CSLItem): void {
+		this.inputEl.value = item.id;
+		this.inputEl.trigger("input");
+		this.close();
+	}
+}
